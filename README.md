f56240bf0279e7e98eba1fad79321f6fe507b5b4
Simon Tegg <simon@loomio.org>
2014-08-07 09:12:21 +1200

    GET groups/

---
+++ README.md
@@ -0,0 +1,9 @@
+
+fake API (GET only) for OpenApp people and group services
+
+```
+git clone git@github.com:open-app/fake-api.git
+cd fake-api
+npm install
+npm run develop
+```
---
+++ index.js
@@ -0,0 +1,34 @@
+var Person = require('oa-types/lib').Person.allOf[1];
+var Group = require('oa-types/lib').Group.allOf[1];
+var mock = require('json-schema-mock');
+var express = require('express');
+var Promise = require('bluebird');
+
+var app = express();         
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080; 
+var router = express.Router();    
+
+router.get('/groups', function(req, res) {
+  var n = parseInt(req.query.n) || 10;
+  var list = new Array(n)
+
+  Promise.map(list, function() {
+    return mock(Group);
+  }).then(function(result) {
+    res.json({ graph: result }); 
+  });
+
+});
+
+
+
+
+app.use('/', router);
+
+app.listen(port);
+console.log('Listening on port ' + port);
