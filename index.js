bc49a5a3e7aa77e45e50eb07f6b06e6290dec8d6
Simon Tegg <simon@loomio.org>
2014-08-10 23:38:54 +1200

    fill out memberships

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 var Person = require('oa-types/lib').Person.allOf[1];
 var Group = require('oa-types/lib').Group.allOf[1];
+var Membership = require('oa-types/lib').Membership;
 var mock = require('json-schema-mock');
 var express = require('express');
 var Promise = require('bluebird');
+var makeMemberships = require('./lib/makeMemberships');
+var makeAgents = require('./lib/makeAgents');
+var assignMemberships = require('./lib/assignMemberships');
+var assignGroupMemberships = require('./lib/assignGroupMemberships')
+var _ = Promise.promisifyAll(require('lodash'));
+
 
 var app = express();         
 var bodyParser = require('body-parser');
@@ -13,29 +20,64 @@ app.use(bodyParser.json());
 var port = process.env.PORT || 8080; 
 var router = express.Router();    
 
+Promise.props({
+  people: makeAgents(Person, 10),
+  groups: makeAgents(Group, 10)
+}).then(function(results) {
+  return  Promise.props({
+    people: results.people,
+    groups: results.groups,
+    memberships: makeMemberships(results.people, results.groups)
+  })
+}).then(function(results) {
+  return Promise.props({
+    people: assignMemberships(results.people, results.memberships),
+    groups: results.groups,
+    memberships: results.memberships
+  })
+}).then(function(results) {
+  return Promise.props({
+    people: results.people,
+    groups: assignGroupMemberships(results.groups, results.memberships)
+  })
+}).then(function(results) {
+
+
 router.get('/groups', function(req, res) {
-  var n = parseInt(req.query.n) || 10;
-  var list = new Array(n)
-
-  Promise.map(list, function() {
-    return mock(Group);
-  }).then(function(result) {
-    res.json({ graph: result }); 
-  });
-
+  res.json({ '@graph': results.groups }); 
 });
 
 router.get('/people', function(req, res) {
-  var n = parseInt(req.query.n) || 10;
-  var list = new Array(n)
-
-  Promise.map(list, function() {
-    return mock(Person);
-  }).then(function(result) {
-    res.json({ graph: result }); 
-  });
-
+  if (req.query.email) {
+    Promise.filter(results.people, function(person) {
+      return person.email === req.query.email
+    }).then(function(people) {
+      res.json({'@graph': people})
+    })
+  } else {
+    res.json({'@graph': results.people})
+  }
 });
+
+
+
+
+})
+
+
+
+
+
+
+// router.get('/groups/:id', function(req, res) {
+//   group = mock(Group);
+//   res.json(group);
+// })
+
+// router.get('/people/:id', function(req, res) {
+//   var person = mock(Person)
+//   res.json(person);
+// })
 
 
 app.use('/', router);
